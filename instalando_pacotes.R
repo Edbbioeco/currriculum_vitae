4582014fdf58007087c62fb9b6b3db1fa16aede9
Edson Silva-Júnior <edsonbbiologia@gmail.com>
2025-12-08 13:27:11 -0300

    Add files via upload

---
+++ instalando_pacotes.R
@@ -0,0 +1,9 @@
+# Instalando o pacote vitae ----
+
+remotes::install_github("mitchelloharawild/vitae")
+
+# Instalando o pacote tinytex ----
+
+install.packages('tinytex')
+
+tinytex::install_tinytex()
