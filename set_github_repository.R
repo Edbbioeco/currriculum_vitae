cbf46a4d866bf9105f7a051ffb53f893f353ec5f
Edbbioeco <edsonbbiologia@gmail.com>
2026-01-06 12:07:24 -0300

    Script to set github repository

---
+++ set_github_repository.R
@@ -0,0 +1,24 @@
+# Pacotes ----
+
+library(usethis)
+
+# Iniciando ----
+
+usethis::use_git()
+
+# Configure o usuario e email ----
+
+usethis::use_git_config(user.name = "Edbbioeco",
+                        user.email = "edsonbbiologia@gmail.com")
+
+# Settando o repositório ----
+
+usethis::proj_get()
+
+usethis::use_git_remote(name = "origin",
+                        url = "https://github.com/Edbbioeco/currriculum_vitaes.git",
+                        overwrite = TRUE)
+
+# Renomear o branch do master para main ----
+
+usethis::git_default_branch_rename(from = "master", to = "main")
